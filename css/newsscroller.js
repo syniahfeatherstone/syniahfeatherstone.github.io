7560d5ca549114cee2ba2a851ceb9d961fac0c78
syniah <syniahfeatherstone@gmail.com>
2016-07-29 17:17:40 -0700

    inital commit

---
+++ css/newsscroller.js
@@ -0,0 +1,2 @@
+<script src="//code.jquery.com/jquery.min.js"> </script>
+<script src="jquery.webticker.js"> </script>
---
+++ js/aboutme.js
@@ -0,0 +1,48 @@
+function  mouseover1 () {
+  var image1 = document.getElementById("syniah");
+  image1.style.width = "300px";
+  image1.style.height = "350px";
+}
+
+function mouseout1 () {
+  var image1 = document.getElementById("syniah");
+  image1.style.width = "300px";
+  image1.style.height = "250x";
+}
+
+function  mouseover2 () {
+  var image2 = document.getElementById("jassy");
+  image2.style.width = "300px";
+  image2.style.height = "350px";
+}
+
+function mouseout2 () {
+  var image2 = document.getElementById("jassy");
+  image2.style.width = "300px";
+  image2.style.height = "250px";
+}
+
+function  mouseover3 () {
+  var image3 = document.getElementById("ayanna");
+  image3.style.width = "300px";
+  image3.style.height = "350px";
+}
+
+function mouseout3 () {
+  var image3= document.getElementById("ayanna");
+  image3.style.width = "300px";
+  image3.style.height = "250px";
+}
+
+function  mouseover4 () {
+  var image4 = document.getElementById("rayvon");
+  image4.style.width = "300px";
+  image4.style.height = "350px";
+}
+
+function mouseout4 () {
+  var image4 = document.getElementById("rayvon");
+
+  image4.style.width = "300px";
+  image4.style.height = "250px";
+}
